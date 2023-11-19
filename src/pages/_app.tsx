5c05d62a2fa1bd2f360bdf2784697955c7044301
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2023-11-19 22:59:40 +0530

    Images,PostView and style up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
+const inter = Inter({ subsets: ["latin"] });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <main className={inter.className}>
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </main>
   );
 };
 
