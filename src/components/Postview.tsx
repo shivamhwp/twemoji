83c491472aaa8420fb8bfa2ee19f3117356aed08
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2023-11-21 14:06:38 +0530

    modify on-clicks

--- src/components/Postview.tsx
+++ src/components/Postview.tsx
@@ -10,27 +10,37 @@ type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 export const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4 ">
-      <Image
-        src={author.profilePicture}
-        width={56}
-        height={56}
-        alt="post-image"
-        className="rounded-full"
-      />
-      <div className="flex flex-col ">
-        <div className="flex gap-1   text-slate-300">
-          <Link href={`@${author.username}`}>
-            <span>{`@${author.username}`}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">{`  ·  ${dayjs(
-              post.createdAt,
-            ).fromNow()}`}</span>
-          </Link>
+    <>
+      <Link href={`/post/${post.id}`}>
+        <div
+          key={post.id}
+          className="flex gap-3 border-b border-slate-400 p-4 "
+        >
+          <div>
+            <Link href={`@${author.username}`}>
+              <Image
+                src={author.profilePicture}
+                width={56}
+                height={56}
+                alt="post-image"
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+          <div className="flex flex-col ">
+            <div className="flex gap-1   text-slate-300">
+              <Link href={`@${author.username}`}>
+                <span>{`@${author.username}`}</span>
+              </Link>
+
+              <span className="font-thin">{`  ·  ${dayjs(
+                post.createdAt,
+              ).fromNow()}`}</span>
+            </div>
+            <span className="text-2xl">{post.content}</span>
+          </div>
         </div>
-        <span className="text-2xl">{post.content}</span>
-      </div>
-    </div>
+      </Link>
+    </>
   );
 };
